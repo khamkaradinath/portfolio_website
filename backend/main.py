77ab0813af783ba411b63b8ee50d229f7eb7014e
AdinathKhamkar <adinath.khamkar97@gmail.com>
2025-11-30 20:20:21 +0530

    second commit

--- backend/main.py
+++ backend/main.py
@@ -42,6 +42,7 @@ app.include_router(auth.router)
 app.include_router(blog.router)
 app.include_router(projects.router)
 app.include_router(interactions.router)
+# hellow
 
 @app.get("/")
 def read_root():
